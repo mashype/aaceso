3686ca92161ef1bf6351186fe0cda906c3005640
mashype <mashype@me.com>
2015-10-31 23:49:20 -0500

    added dropdown for usertype

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151101042640) do
+ActiveRecord::Schema.define(version: 20151101044437) do
 
   create_table "licenses", force: :cascade do |t|
     t.string   "state"
@@ -113,10 +113,16 @@ ActiveRecord::Schema.define(version: 20151101042640) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.integer  "usertype"
+    t.integer  "usertype_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "usertypes", force: :cascade do |t|
+    t.string   "usertypename"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-specialties = Specialty.create ([{specialty: 'Internal Medicine'}, {specialty: 'Anesthesia'}, {specialty: 'Pathology'}])
+usertypes = Usertype.create ([{usertypename: 'Provider'}, {usertypename: 'Patient'}, {usertypename: 'Provider Admin'}, {usertypename: 'Master Admin'}])
